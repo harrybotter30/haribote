aee6c9176c66964c60d3c0bf549c90a6c5270c4a
harrybotter30 <harry.botter.30@gmail.com>
2017-06-25 12:32:47 +0900

    project harib22f (25th day)

--- bootpack.h
+++ bootpack.h
@@ -210,6 +210,8 @@ struct TASK {
 	int level, priority;
 	struct FIFO32 fifo;
 	struct TSS32 tss;
+	struct CONSOLE *cons;
+	int ds_base;
 };
 struct TASKLEVEL {
 	int running; /* “®ì‚µ‚Ä‚¢‚éƒ^ƒXƒN‚Ì” */
