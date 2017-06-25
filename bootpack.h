f1887db836585701fe1987da1a807413cfa5014e
harrybotter30 <harry.botter.30@gmail.com>
2017-06-25 12:32:44 +0900

    project harib07a (10th day)

--- bootpack.h
+++ bootpack.h
@@ -121,3 +121,21 @@ void inthandler2c(int *esp);
 void enable_mouse(struct MOUSE_DEC *mdec);
 int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 extern struct FIFO8 mousefifo;
+
+/* memory.c */
+#define MEMMAN_FREES		4090	/* ‚±‚ê‚Å–ñ32KB */
+#define MEMMAN_ADDR			0x003c0000
+struct FREEINFO {	/* ‚ ‚«î•ñ */
+	unsigned int addr, size;
+};
+struct MEMMAN {		/* ƒƒ‚ƒŠŠÇ— */
+	int frees, maxfrees, lostsize, losts;
+	struct FREEINFO free[MEMMAN_FREES];
+};
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
