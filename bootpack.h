f692835be7c33303775d7a7d9e9923cc218e2760
harry botter <harry.botter.30@gmail.com>
2022-04-29 16:50:29 +0900

    add memman_alloc_4k() and memman_free_4k()

--- bootpack.h
+++ bootpack.h
@@ -137,3 +137,5 @@ void memman_init(struct MEMMAN *man);
 unsigned int memman_total(struct MEMMAN *man);
 unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
